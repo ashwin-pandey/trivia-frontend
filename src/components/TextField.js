77392a674658aedbe626a4aac7a55117d08df963
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2023-03-25 14:16:19 +0530

    added a text field component

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -5,7 +5,7 @@ const TextField = (props) => {
     const handleChange = () => {}
     return (
         <div>
-            <div className='form-inline form-horizontal'>
+            <div className='form-inline form-horizontal mt-3'>
                 <label htmlFor="Number of questions?"><b>Number of questions?</b></label>
                 <div className="form-group">
                     <input type='number' className='form-control' label='Number of questions?' onChange={handleChange} />
